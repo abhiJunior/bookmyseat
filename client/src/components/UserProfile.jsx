92c3f9f9fa2cfe7386e59d5193b4b775aabadcc0
Abhishek <149757033+abhiJunior@users.noreply.github.com>
2025-09-20 13:48:31 +0530

    Update UserProfile.jsx

--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function UserProfile() {
-  const url = "http://localhost:5000"
+  const url = "https://bookmyseat-backend.onrender.com"
   const navigate = useNavigate();
   const [user, setUser] = useState({});
 
