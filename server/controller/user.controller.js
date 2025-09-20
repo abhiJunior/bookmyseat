83030bf1026e8a1ba938d53314d802629b3f3987
Abhishek <149757033+abhiJunior@users.noreply.github.com>
2025-09-20 14:56:11 +0530

    Update user.controller.js

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -38,7 +38,7 @@ export const login = async(req,res)=>{
         const jwtToken = await validUser.generateJWTToken()
 
         // PUT JWT token in cookie
-        res.cookie("token", token, {
+        res.cookie("token", jwtToken, {
           httpOnly: true,
           secure: process.env.NODE_ENV === "production", 
           sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
